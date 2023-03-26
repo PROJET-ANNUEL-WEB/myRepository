404e9367d261c331af1f69f615ff548573f10d21
Nolan1342 <127830667+Nolan1342@users.noreply.github.com>
2023-03-26 12:43:22 +0200

    Add files via upload
    Mes fichiers le bro

--- pageadmin.php
+++ pageadmin.php
@@ -1,21 +1,17 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html>
 
 <head>
-    <title>Comptable</title>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!--===============================================================================================-->
-    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
-    <!--===============================================================================================-->
-    <link href="csscompta.css?<?php echo time(); ?>" rel="stylesheet" <!--===============================================================================================-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!--===============================================================================================-->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.3/css/dataTables.bootstrap.min.css">
+    <link href="csscompta.css" rel="stylesheet" id="bootstrap-css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <title> Page admin </title>
 </head>
-<header>
+
+<body>
     <nav class="navbar navbar-default navbar-static-top">
         <div class="container-fluid">
             <!-- Brand and toggle get grouped for better mobile display -->
@@ -30,7 +26,7 @@
                     <span class="icon-bar"></span>
                 </button>
                 <a class="navbar-brand" href="#">
-                    Administrator
+                    Administrateur
                 </a>
             </div>
 
@@ -61,177 +57,145 @@
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
     </nav>
-</header>
+    <div class="container-fluid main-container">
+        <div class="col-md-2 sidebar">
+            <div class="row">
+                <!-- uncomment code for absolute positioning tweek see top comment in css -->
+                <div class="absolute-wrapper"> </div>
+                <!-- Menu -->
+                <div class="side-menu">
+                    <nav class="navbar navbar-default" role="navigation">
+                        <!-- Main Menu -->
+                        <div class="side-menu-container">
+                            <ul class="nav navbar-nav">
+                                <li class="active"><a href="#"><span class="glyphicon glyphicon-dashboard"></span> Dashboard</a></li>
+                                <li><a href="#"><span class="glyphicon glyphicon-plane"></span> Active Link</a></li>
+                                <li><a href="#"><span class="glyphicon glyphicon-cloud"></span> Link</a></li>
 
-<body>
+                                <!-- Dropdown-->
+                                <li class="panel panel-default" id="dropdown">
+                                    <a data-toggle="collapse" href="#dropdown-lvl1">
+                                        <span class="glyphicon glyphicon-user"></span> Sub Level <span class="caret"></span>
+                                    </a>
 
+                                    <!-- Dropdown level 1 -->
+                                    <div id="dropdown-lvl1" class="panel-collapse collapse">
+                                        <div class="panel-body">
+                                            <ul class="nav navbar-nav">
+                                                <li><a href="#">Link</a></li>
+                                                <li><a href="#">Link</a></li>
+                                                <li><a href="#">Link</a></li>
 
-    <section id="content">
+                                                <!-- Dropdown level 2 -->
+                                                <li class="panel panel-default" id="dropdown">
+                                                    <a data-toggle="collapse" href="#dropdown-lvl2">
+                                                        <span class="glyphicon glyphicon-off"></span> Sub Level <span class="caret"></span>
+                                                    </a>
+                                                    <div id="dropdown-lvl2" class="panel-collapse collapse">
+                                                        <div class="panel-body">
+                                                            <ul class="nav navbar-nav">
+                                                                <li><a href="#">Link</a></li>
+                                                                <li><a href="#">Link</a></li>
+                                                                <li><a href="#">Link</a></li>
+                                                            </ul>
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </li>
 
-        <div class="container autumn-text  ">
-            <h1>FRAIS A TRAITER</h1>
-            <table id="table1" class="autumn-text1" style="width: 100%">
-                <thead>
-                    <tr>
-                        <th scope="col">Créateur</th>
-                        <th scope="col">Type</th>
-                        <th scope="col">Prix</th>
-                        <th scope="col">Date</th>
-                        <th scope="col">Objet</th>
-                        <th scope="col">Valider</th>
-                        <th scope="col">Refuser</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    $base = new PDO('mysql:host=localhost;dbname=projetannuel;charset=utf8mb4', 'root', '');
-                    $donnees = $base->query("SELECT utilisateur.Nom ,frais.ID_Frais, type_de_frais.idType , frais.Date_de_frais , frais.Montant , frais.objet , etat_du_frais.Id_Etat FROM frais JOIN utilisateur ON frais.ID_utilisateur=utilisateur.ID_utilisateur JOIN Type_de_frais ON frais.idType=Type_de_frais.idType JOIN etat_du_frais ON frais.Id_Etat=etat_du_frais.Id_Etat ORDER BY frais.Date_de_frais DESC")->fetchAll();
+                                <li><a href="#"><span class="glyphicon glyphicon-signal"></span> Link</a></li>
 
-                    foreach ($donnees as $row) {
+                            </ul>
+                        </div><!-- /.navbar-collapse -->
+                    </nav>
 
-                        if ($row['Id_Etat'] == 3) {
-
-                    ?>
-                            <tr>
-                                <td>
-                                    <h5><?= $row['Nom']; ?></h5>
-                                </td>
-                                <td>
-                                    <h5><?= $row['idType'] ?></h5>
-                                </td>
-                                <td>
-                                    <h5><?= $row['Montant'] ?></h5>
-                                </td>
-                                <td>
-                                    <h5><?= $row['Date_de_frais'] ?></h5>
-                                </td>
-                                <td>
-                                    <h5><?= $row['objet'] ?></h5>
-                                </td>
-
-                                <!--Nous alons mettre l'id de chaque employé dans ce lien -->
-                                <td><a href="valide.php?id=<?= $row['ID_Frais'] ?>"><img src="images/icons/valid.png" width=30px height=auto></a></td>
-                                <td><a href="refus.php?id=<?= $row['ID_Frais'] ?>"><img src="images/icons/trash.png" width=30px height=auto></a></td>
-                            </tr>
-                    <?php
-                        }
-                    }
-
-                    ?>
-                </tbody>
-            </table>
+                </div>
+            </div>
         </div>
+        <div class="col-md-10 content">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Gestion des rôles
+                </div>
+                <div class="panel-body">
+                     - Ajouter un rôle <br>- Modifier un rôle <br> - Supprimer un rôle
+                </div>
+            </div>
+        </div>
+        <div class="col-md-10 content">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Gestion des utilisateurs
+                </div>
+                <div class="panel-body">
+                </head>
+<body>
+<div class="container">
+<h3>Enregistrer un utilisateur</h3>
+    <div class="row">
+        <form action="pageadmin.php" method="post">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
+            </div>
+            <div class="mb-3">
+                <label for="mot_de_passe" class="form-label">Mot de passe</label>
+                <input name="mot_de_passe" type="text" class="form-control" id="mot_de_passe">
+            </div>
 
+            <div class="mb-3">
+                <label for="IDRole" class="form-label">Numéro de Rôle</label>
+                <input type="text" name="ID_Role" class="form-control" id="IDRole">
+            </div>
 
-        <section id="content">
+            <div class="mb-3">
+                <label for="nom" class="form-label">Nom</label>
+                <input type="text" name="Nom" class="form-control" id="nom">
+            </div>
 
-            <div class="container autumn-text">
-                <h1>FRAIS DEJA TRAITES</h1>
-                <table id="table2" class="table hover autumn-text1" style="width: 100%">
-                    <thead>
-                        <tr>
-                            <th scope="col">Créateur</th>
-                            <th scope="col">Type</th>
-                            <th scope="col">Prix</th>
-                            <th scope="col">Date</th>
-                            <th scope="col">Objet</th>
-                            <th scope="col">Etat</th>
-                            <th scope="col">Modifier</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        <?php
-
-                        $base = new PDO('mysql:host=localhost;dbname=projetannuel;charset=utf8mb4', 'root', '');
-
-                        $donnees = $base->query("SELECT utilisateur.Nom ,frais.ID_Frais, type_de_frais.idType , frais.Date_de_frais , frais.Montant , frais.objet , etat_du_frais.Id_Etat FROM frais JOIN utilisateur ON frais.ID_utilisateur=utilisateur.ID_utilisateur JOIN Type_de_frais ON frais.idType=Type_de_frais.idType JOIN etat_du_frais ON frais.Id_Etat=etat_du_frais.Id_Etat ORDER BY frais.Date_de_frais DESC")->fetchAll();
-
-                        foreach ($donnees as $row)
-
-
-                            if ($row['Id_Etat'] != 3) {
-
-                        ?>
-                            <tr>
-                                <td>
-                                    <h5><?= $row['Nom']; ?></h5>
-                                </td>
-                                <td>
-                                    <h5><?= $row['idType'] ?></h5>
-                                </td>
-                                <td>
-                                    <h5><?= $row['Montant'] ?></h5>
-                                </td>
-                                <td>
-                                    <h5><?= $row['Date_de_frais'] ?></h5>
-                                </td>
-                                <td>
-                                    <h5><?= $row['objet'] ?></h5>
-                                </td>
-
-                                <?php
-                                foreach ($donnees as $row) {
-                                    if ($row['Id_Etat'] != 3)
-                                        if ($row['Id_Etat'] == 1) {
-                                ?>
-                                        <td><img src="images/icons/valid.png" width=30px height=auto></a></td>
-                                        <td><a href="refus.php?id=<?= $row['ID_Frais'] ?>"><img src="images/icons/trash.png" width=30px height=auto></a></td>
-                            </tr>
-                        <?php
-                                        } else if ($row['Id_Etat'] == 2) {
-                        ?>
-                            <td><img src="images/icons/trash.png" width=30px height=auto></a></td>
-                            <td><a href="valide.php?id=<?= $row['ID_Frais'] ?>"><img src="images/icons/valid.png" width=30px height=auto></a></td>
-                            </tr>
-                        <?php
-
-                                        }
-
-                        ?>
-
-
-                <?php
-                                }
-                            }
-
-                ?>
-                    </tbody>
-                </table>
+            <div class="mb-3">
+                <label for="prenom" class="form-label">Prénom</label>
+                <input type="text" name="Prenom" class="form-control" id="prenom">
             </div>
 
 
+            <input type="submit" class="btn btn-primary" value="Enregistrer"></input>
 
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+<?php
+    if (isset($_POST['email']) && isset($_POST['mot_de_passe']) && isset($_POST['ID_Role']) && isset($_POST['Nom']) && isset($_POST['Prenom'])){
+        $email = $_POST['email'];
+        $mot_de_passe = $_POST['mot_de_passe'];
+        $ID_Role = $_POST['ID_Role'];
+        $Nom = $_POST['Nom'];
+        $Prenom = $_POST['Prenom'];
+
+        $db = new PDO('mysql:host=localhost;dbname=projetannuel;charset=utf8', 'root', '');
+
+        $stmt = $db->prepare("INSERT INTO utilisateur (email, mot_de_passe, Nom, Prenom,ID_Role) VALUES (:email, :mot_de_passe, :Nom, :Prenom, :ID_Role)");
+        $stmt->bindParam(':mot_de_passe', $mot_de_passe);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':Prenom', $Prenom);
+        $stmt->bindParam(':Nom', $Nom);
+        $stmt->bindParam(':ID_Role', $ID_Role);
+
+        $stmt->execute();
+
+        echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>C est nickel</strong> l ajout est OK.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>';
+}
+?>
+
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
 
-            <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
-            <script src="https://cdn.datatables.net/1.13.3/js/jquery.dataTables.js"></script>
 
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-            <script type="text/javascript" src="https://cdn.datatables.net/1.13.3/js/dataTables.bootstrap.min.js"></script>
-
-            <script type="text/javascript">
-                $(document).ready(function() {
-                    $('#table1').DataTable({
-                        "language": {
-                            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/French.json"
-                        }
-                    });
-                });
-            </script>
-
-            <script type="text/javascript">
-                $(document).ready(function() {
-                    $('#table2').DataTable({
-                        "language": {
-                            "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/French.json"
-                        }
-                    });
-                });
-            </script>
 
 </body>
 
